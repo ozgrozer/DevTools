41c97e9f46e50d0430a58f55f514adbb802f5d38
mac <ozgr@live.com>
2024-06-22 19:07:56 +0300

    Fix padding issue

--- src/views/tools/UrlEncodeDecode.jsx
+++ src/views/tools/UrlEncodeDecode.jsx
@@ -3,21 +3,25 @@ import { View, TextInput, StyleSheet } from 'react-native'
 export default () => {
   return (
     <View style={styles.contentWrapper}>
-      <TextInput
-        multiline
-        placeholder='Input'
-        enableFocusRing={false}
-        style={styles.textInput}
-        placeholderTextColor='#888'
-      />
+      <View style={styles.inputWrapper}>
+        <TextInput
+          multiline
+          placeholder='Input'
+          enableFocusRing={false}
+          style={styles.textInput}
+          placeholderTextColor='#888'
+        />
+      </View>
 
-      <TextInput
-        multiline
-        placeholder='Output'
-        enableFocusRing={false}
-        style={styles.textInput}
-        placeholderTextColor='#888'
-      />
+      <View style={styles.inputWrapper}>
+        <TextInput
+          multiline
+          placeholder='Output'
+          enableFocusRing={false}
+          style={styles.textInput}
+          placeholderTextColor='#888'
+        />
+      </View>
     </View>
   )
 }
@@ -28,9 +32,12 @@ const styles = StyleSheet.create({
     gap: 24,
     padding: 24
   },
+  inputWrapper: {
+    flex: 1
+  },
   textInput: {
+    flex: 1,
     padding: 16,
-    height: '50%',
     paddingTop: 12,
     borderRadius: 6,
     paddingBottom: 12,
