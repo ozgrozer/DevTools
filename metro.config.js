78114f74ff4e116db2a4f1d06b746995195cc1f6
mac <ozgr@live.com>
2024-06-22 13:16:49 +0300

    Fix standard.js

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,3 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { mergeConfig, getDefaultConfig } = require('@react-native/metro-config')
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(getDefaultConfig(__dirname), {})
