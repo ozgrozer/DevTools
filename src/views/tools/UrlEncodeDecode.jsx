88fbee07f4d1c400163e2210f0b7fca9217ca55a
mac <ozgr@live.com>
2024-06-22 19:04:28 +0300

    Add text inputs

--- src/views/tools/UrlEncodeDecode.jsx
+++ src/views/tools/UrlEncodeDecode.jsx
@@ -1,15 +1,40 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, TextInput, StyleSheet } from 'react-native'
 
 export default () => {
   return (
     <View style={styles.contentWrapper}>
-      <Text>UrlEncodeDecode</Text>
+      <TextInput
+        multiline
+        placeholder='Input'
+        enableFocusRing={false}
+        style={styles.textInput}
+        placeholderTextColor='#888'
+      />
+
+      <TextInput
+        multiline
+        placeholder='Output'
+        enableFocusRing={false}
+        style={styles.textInput}
+        placeholderTextColor='#888'
+      />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   contentWrapper: {
-    flex: 1
+    flex: 1,
+    gap: 24,
+    padding: 24
+  },
+  textInput: {
+    height: '50%',
+    paddingTop: 12,
+    borderRadius: 6,
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingBottom: 12,
+    backgroundColor: '#312E34'
   }
 })
