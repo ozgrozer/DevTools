0ff867c5cc7c028d1c191715c2d0ca025a08445f
mac <ozgr@live.com>
2024-06-22 18:40:45 +0300

    Add buttons

---
+++ src/functions/buttons.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    id: 1,
+    title: 'URL Encode/Decode',
+    component: 'UrlEncodeDecode'
+  },
+  {
+    id: 2,
+    title: 'URL Parser',
+    component: 'UrlParser'
+  }
+]
